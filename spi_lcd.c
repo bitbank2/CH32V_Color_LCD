587f4b45ab1c33b86e2a958b503ab2006409c0f0
Laurence Bank <laurencebank@Laurences-Air.home>
2023-09-16 13:01:55 +0100

    Added support for more LCDs

--- spi_lcd.c
+++ spi_lcd.c
@@ -1,25 +1,59 @@
-/*
- * spi_lcd.c
- * a ST7735S LCD display library
- *  Created on: Sep 11, 2023
- *      written by Larry Bank
- */
+//
+// spi_lcd.c
+// a Sitronix LCD display library
+// Created on: Sep 11, 2023
+// written by Larry Bank (bitbank@pobox.com)
+//
+//
+// Copyright 2023 BitBank Software, Inc. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//===========================================================================
+//
+
 #include "Arduino.h"
 #include "spi_lcd.h"
 
 static uint8_t u8CS, u8DC, u8BL;
+static uint8_t u8MADCTL; // original value
 static int iCursorX, iCursorY;
-// Kludge - the system is reporting the wrong amount of SRAM (10K) when it is 20K, so hard code the addresses
-#define u8Cache0 (SRAM_BASE+0x2800)
-#define u8Cache1 (SRAM_BASE+0x3c00)
-static uint8_t *pCache0 = (uint8_t *)u8Cache0;
-static uint8_t *pCache1 = (uint8_t *)u8Cache1;
-//static uint8_t u8Cache0[CACHE_SIZE]; // ping-pong data buffers
-//static uint8_t u8Cache1[CACHE_SIZE];
-//static uint8_t *pCache0 = u8Cache0, *pCache1 = u8Cache1;
+static int iNativeWidth, iNativeHeight, iNativeXOff, iNativeYOff, iLCDWidth, iLCDHeight, iLCDPitch, iLCDXOff, iLCDYOff;
+static uint8_t u8Cache0[CACHE_SIZE]; // ping-pong data buffers
+static uint8_t u8Cache1[CACHE_SIZE];
+static uint8_t *pCache0 = u8Cache0, *pCache1 = u8Cache1;
 volatile int bDMA = 0;
 
-const unsigned char uc80InitList[] = {
+const uint8_t uc240x240InitList[] = {
+    1, 0x13, // partial mode off
+    1, 0x21, // display inversion off
+    2, 0x36,0x60,    // memory access 0xc0 for 180 degree flipped
+    2, 0x3a,0x55,    // pixel format; 5=RGB565
+    3, 0x37,0x00,0x00, //
+    6, 0xb2,0x0c,0x0c,0x00,0x33,0x33, // Porch control
+    2, 0xb7,0x35,    // gate control
+    2, 0xbb,0x1a,    // VCOM
+    2, 0xc0,0x2c,    // LCM
+    2, 0xc2,0x01,    // VDV & VRH command enable
+    2, 0xc3,0x0b,    // VRH set
+    2, 0xc4,0x20,    // VDV set
+    2, 0xc6,0x0f,    // FR control 2
+    3, 0xd0, 0xa4, 0xa1,     // Power control 1
+    15, 0xe0, 0x00,0x19,0x1e,0x0a,0x09,0x15,0x3d,0x44,0x51,0x12,0x03,
+        0x00,0x3f,0x3f,     // gamma 1
+    15, 0xe1, 0x00,0x18,0x1e,0x0a,0x09,0x25,0x3f,0x43,0x52,0x33,0x03,
+        0x00,0x3f,0x3f,        // gamma 2
+    1, 0x29,    // display on
+    0
+};
+
+const uint8_t uc80InitList[] = {
     2, 0x3a, 0x05,    // pixel format RGB565
     2, 0x36, 0x68, // MADCTL (0/90/180/270 and color/inversion)
     17, 0xe0, 0x09, 0x16, 0x09,0x20,
@@ -35,6 +69,36 @@ const unsigned char uc80InitList[] = {
     0
 };
 
+const uint8_t uc160InitList[] = {
+        2, 0x3a, 0x05,  // pixel format RGB565
+        2, 0x36, 0x60, // MADCTL
+        17, 0xe0, 0x09, 0x16, 0x09,0x20,
+                0x21,0x1b,0x13,0x19,
+                0x17,0x15,0x1e,0x2b,
+                0x04,0x05,0x02,0x0e, // gamma sequence
+        17, 0xe1, 0x0b,0x14,0x08,0x1e,
+                0x22,0x1d,0x18,0x1e,
+                0x1b,0x1a,0x24,0x2b,
+                0x06,0x06,0x02,0x0f,
+			    1, 0x20,    // display inversion off
+				1, 0x29, // display on
+        0
+};
+const uint8_t uc128InitList[] = {
+        2, 0x3a, 0x05,  // pixel format RGB565
+        2, 0x36, 0x68, // MADCTL
+        17, 0xe0, 0x09, 0x16, 0x09,0x20,
+                0x21,0x1b,0x13,0x19,
+                0x17,0x15,0x1e,0x2b,
+                0x04,0x05,0x02,0x0e, // gamma sequence
+        17, 0xe1, 0x0b,0x14,0x08,0x1e,
+                0x22,0x1d,0x18,0x1e,
+                0x1b,0x1a,0x24,0x2b,
+                0x06,0x06,0x02,0x0f,
+			    1, 0x20,    // display inversion off
+				1, 0x29, // display on
+        0
+};
 const uint8_t ucFont[]PROGMEM = {
   0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x06,0x5f,0x5f,0x06,0x00,
   0x00,0x07,0x07,0x00,0x07,0x07,0x00,0x14,0x7f,0x7f,0x14,0x7f,0x7f,0x14,
@@ -267,12 +331,72 @@ void lcdWriteDATA(uint8_t *pData, int iLen)
 	}
 } /* lcdWriteDATA() */
 
-void lcdInit(uint32_t u32Speed, uint8_t u8CSPin, uint8_t u8DCPin, uint8_t u8RSTPin, uint8_t u8BLPin)
+void lcdInit(int iLCDType, uint32_t u32Speed, uint8_t u8CSPin, uint8_t u8DCPin, uint8_t u8RSTPin, uint8_t u8BLPin)
 {
 //    uint8_t iBGR = 0;
-	uint8_t *s;
+	uint8_t *s = NULL;
 	int iCount;
 
+	if (iLCDType < 0 || iLCDType >= LCD_COUNT) return;
+	switch (iLCDType) {
+	case LCD_ST7789_135x240:
+		iNativeWidth = iLCDWidth = 240; // initialize in landscape mode
+		iNativeHeight = iLCDHeight = 135;
+		iNativeXOff = iLCDXOff = 40;
+		iNativeYOff = iLCDYOff = 53;
+		u8MADCTL = 0x60;
+	    s = (unsigned char *)uc240x240InitList;
+		break;
+	case LCD_ST7789_172x320:
+		iNativeWidth = iLCDWidth = 320; // initialize in landscape mode
+		iNativeHeight = iLCDHeight = 172;
+		iNativeXOff = iLCDXOff = 0;
+		iNativeYOff = iLCDYOff = 34;
+		u8MADCTL = 0x60;
+	    s = (unsigned char *)uc240x240InitList;
+		break;
+	case LCD_ST7789_240x280:
+		iNativeWidth = iLCDWidth = 280; // initialize in landscape mode
+		iNativeHeight = iLCDHeight = 240;
+		iNativeXOff = iLCDXOff = 20;
+		iNativeYOff = iLCDYOff = 0;
+		u8MADCTL = 0x60;
+	    s = (unsigned char *)uc240x240InitList;
+		break;
+	case LCD_ST7735_80x160:
+		iNativeWidth = iLCDWidth = 160; // initialize in landscape mode
+		iNativeHeight = iLCDHeight = 80;
+		iNativeXOff = iLCDXOff = 0;
+		iNativeYOff = iLCDYOff = 24;
+		u8MADCTL = 0x68;
+	    s = (unsigned char *)uc80InitList;
+		break;
+	case LCD_GC9107_128x128:
+		iNativeWidth = iLCDWidth = 128;
+		iNativeHeight = iLCDHeight = 128;
+		iNativeXOff = iLCDXOff = 1;
+		iNativeYOff = iLCDYOff = 2;
+		u8MADCTL = 0x68;
+	    s = (unsigned char *)uc240x240InitList;
+		break;
+	case LCD_ST7735_128x128:
+		iNativeWidth = iLCDWidth = 128;
+		iNativeHeight = iLCDHeight = 128;
+		iNativeXOff = iLCDXOff = 1;
+		iNativeYOff = iLCDYOff = 0;
+		u8MADCTL = 0x68;
+	    s = (unsigned char *)uc128InitList;
+		break;
+	case LCD_ST7735_128x160:
+		iNativeWidth = iLCDWidth = 160;
+		iNativeHeight = iLCDHeight = 128;
+		iNativeXOff = iLCDXOff = 0;
+		iNativeYOff = iLCDYOff = 0;
+		u8MADCTL = 0x60;
+	    s = (unsigned char *)uc160InitList;
+		break;
+	} // switch on LCD type
+	iLCDPitch = iLCDWidth*2;
 	u8CS = u8CSPin;
 	pinMode(u8CSPin, OUTPUT);
 	digitalWrite(u8CSPin, 1);
@@ -294,9 +418,8 @@ void lcdInit(uint32_t u32Speed, uint8_t u8CSPin, uint8_t u8DCPin, uint8_t u8RSTP
 	Delay_Ms(200);
 	lcdWriteCMD(0x11); // sleep out
 	Delay_Ms(100);
-    s = (unsigned char *)uc80InitList;
     iCount = 1;
-     while (iCount)
+     while (s && iCount)
      {
 		 iCount = *s++;
 		 if (iCount != 0)
@@ -310,12 +433,56 @@ void lcdInit(uint32_t u32Speed, uint8_t u8CSPin, uint8_t u8DCPin, uint8_t u8RSTP
 
 } /* lcdInit() */
 
+void lcdOrientation(int iOrientation)
+{
+	uint8_t u8 = u8MADCTL; // original value
+
+	switch (iOrientation) {
+	case ORIENTATION_0: // use original MADCTL value
+		iLCDWidth = iNativeWidth;
+		iLCDHeight = iNativeHeight;
+		iLCDPitch = iLCDWidth * 2;
+		iLCDXOff = iNativeXOff;
+		iLCDYOff = iNativeYOff;
+		break;
+	case ORIENTATION_90:
+		u8 ^= MADCTL_XFLIP;
+		u8 ^= MADCTL_VFLIP;
+		iLCDWidth = iNativeHeight;
+		iLCDHeight = iNativeWidth;
+		iLCDPitch = iLCDWidth * 2;
+		iLCDXOff = iNativeYOff;
+		iLCDYOff = iNativeXOff;
+		break;
+	case ORIENTATION_180:
+		u8 ^= MADCTL_XFLIP;
+		u8 ^= MADCTL_YFLIP;
+		iLCDWidth = iNativeWidth;
+		iLCDHeight = iNativeHeight;
+		iLCDPitch = iLCDWidth * 2;
+		iLCDXOff = iNativeXOff;
+		iLCDYOff = iNativeYOff;
+		break;
+	case ORIENTATION_270:
+		u8 ^= MADCTL_YFLIP;
+		u8 ^= MADCTL_VFLIP;
+		iLCDWidth = iNativeHeight;
+		iLCDHeight = iNativeWidth;
+		iLCDPitch = iLCDWidth * 2;
+		iLCDXOff = iNativeYOff;
+		iLCDYOff = iNativeXOff;
+		break;
+	}
+	 lcdWriteCMD(0x36); // MADCTL
+	 lcdWriteDATA(&u8, 1);
+} /* lcdOrientation() */
+
 void lcdSetPosition(int x, int y, int w, int h)
 {
 uint8_t ucBuf[8];
 
-     x += LCD_XOFFSET;
-     y += LCD_YOFFSET;
+     x += iLCDXOff;
+     y += iLCDYOff;
      ucBuf[0] = (unsigned char)(x >> 8);
      ucBuf[1] = (unsigned char)x;
      x = x + w - 1;
@@ -367,16 +534,16 @@ void lcdFill(uint16_t usData)
 	uint16_t *d;
 
     usData = (usData >> 8) | (usData << 8); // swap hi/lo byte for LCD
-    lcdSetPosition(0,0, LCD_WIDTH, LCD_HEIGHT);
+    lcdSetPosition(0,0, iLCDWidth, iLCDHeight);
     // fit within our temp buffer
-    for (cy = 0; cy < LCD_HEIGHT; cy++) {
+    for (cy = 0; cy < iLCDHeight; cy++) {
     	if (cy < 2) { // both buffers need a copy
     		d = (uint16_t *)pCache0; // pointer swapped after each write
-    		for (cx = 0; cx < LCD_WIDTH; cx++) {
+    		for (cx = 0; cx < iLCDWidth; cx++) {
     			d[cx] = usData;
     		}
     	}
-        lcdWriteDATA(pCache0, LCD_WIDTH*2); // fill with data words
+        lcdWriteDATA(pCache0, iLCDWidth*2); // fill with data words
    } // for y
 
 } /* lcdFill() */
@@ -394,10 +561,10 @@ void spilcdDrawPattern(uint8_t *pPattern, int iSrcPitch, int iDestX, int iDestY,
     uint8_t *s, uc, ucMask;
     uint16_t *d, u16Clr;
 
-     if (iDestX+iCX > LCD_WIDTH) // trim to fit on display
-         iCX = (LCD_WIDTH - iDestX);
-     if (iDestY+iCY > LCD_HEIGHT)
-         iCY = (LCD_HEIGHT - iDestY);
+     if (iDestX+iCX > iLCDWidth) // trim to fit on display
+         iCX = (iLCDWidth - iDestX);
+     if (iDestY+iCY > iLCDHeight)
+         iCY = (iLCDHeight - iDestY);
      if (pPattern == NULL || iDestX < 0 || iDestY < 0 || iCX <=0 || iCY <= 0)
          return;
        u16Clr = (usColor >> 8) | (usColor << 8); // swap low/high bytes
@@ -448,7 +615,7 @@ uint8_t *pFont;
     iLen = strlen(szMsg);
 
     if (iFontSize == FONT_12x16) {
-        if ((12*iLen) + x > LCD_WIDTH) iLen = (LCD_WIDTH - x)/12; // can't display it all
+        if ((12*iLen) + x > iLCDWidth) iLen = (iLCDWidth - x)/12; // can't display it all
         if (iLen < 0) return -1;
         iStride = iLen*12;
         lcdSetPosition(x, y, iStride, 16);
@@ -494,7 +661,7 @@ uint8_t *pFont;
 
     cx = (iFontSize == FONT_8x8) ? 8:6;
     pFont = (iFontSize == FONT_8x8) ? (uint8_t *)ucFont : (uint8_t *)ucSmallFont;
-    if ((cx*iLen) + x > LCD_WIDTH) iLen = (LCD_WIDTH - x)/cx; // can't display it all
+    if ((cx*iLen) + x > iLCDWidth) iLen = (iLCDWidth - x)/cx; // can't display it all
     iStride = iLen * cx*2;
     for (i=0; i<iLen; i++)
     {
@@ -551,7 +718,7 @@ uint16_t *d;
    usBGColor = (usBGColor >> 8) | (usBGColor << 8);
 
    i = 0;
-   while (szMsg[i] && x < LCD_WIDTH)
+   while (szMsg[i] && x < iLCDWidth)
    {
       c = szMsg[i++];
       if (c < font.first || c > font.last) // undefined character
@@ -564,15 +731,15 @@ uint16_t *d;
       cx = pGlyph->width;
       cy = pGlyph->height;
       iBitOff = 0; // bitmap offset (in bits)
-      if (dy + cy > LCD_HEIGHT)
-         cy = LCD_HEIGHT - dy; // clip bottom edge
+      if (dy + cy > iLCDHeight)
+         cy = iLCDHeight - dy; // clip bottom edge
       else if (dy < 0) {
          cy += dy;
          iBitOff += (pGlyph->width * (-dy));
          dy = 0;
       }
-      if (dx + cx > LCD_WIDTH)
-         cx = LCD_WIDTH - dx; // clip right edge
+      if (dx + cx > iLCDWidth)
+         cx = iLCDWidth - dx; // clip right edge
       s = font.bitmap + pGlyph->bitmapOffset; // start of bitmap data
       // Bitmap drawing loop. Image is MSB first and each pixel is packed next
       // to the next (continuing on to the next character line)
@@ -584,11 +751,11 @@ uint16_t *d;
          miny = y + pGlyph->yOffset;
          c = 'y' - font.first;
          maxy = miny + pGlyph->height;
-         if (maxy > LCD_HEIGHT)
-            maxy = LCD_HEIGHT;
+         if (maxy > iLCDHeight)
+            maxy = iLCDHeight;
          cx = pGlyph->xAdvance;
-         if (cx + x > LCD_WIDTH) {
-            cx = LCD_WIDTH - x;
+         if (cx + x > iLCDWidth) {
+            cx = iLCDWidth - x;
          }
          lcdSetPosition(x, miny, cx, maxy-miny);
             // blank out area above character
--- spi_lcd.h
+++ spi_lcd.h
@@ -1,22 +1,51 @@
-/*
- * spi_lcd.h
- *
- *  Created on: Sep 11, 2023
- *      Author: larry
- */
+//
+// spi_lcd.h
+// a Sitronix LCD display library
+// Created on: Sep 11, 2023
+// written by Larry Bank (bitbank@pobox.com)
+//
+//
+// Copyright 2023 BitBank Software, Inc. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//    http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//===========================================================================
+//
 
 #ifndef USER_SPI_LCD_H_
 #define USER_SPI_LCD_H_
 
-#define LCD_WIDTH 160
-#define LCD_PITCH (LCD_WIDTH*2)
-#define LCD_HEIGHT 80
+enum {
+	LCD_ST7735_80x160 = 0,
+	LCD_ST7735_80x160_B,
+	LCD_ST7735_128x128,
+	LCD_ST7735_128x160,
+	LCD_ST7789_135x240,
+	LCD_ST7789_172x320,
+	LCD_ST7789_240x240,
+	LCD_ST7789_240x280,
+	LCD_ST7789_240x320,
+	LCD_GC9107_128x128,
+	LCD_COUNT
+};
+
+enum {
+	ORIENTATION_0 = 0,
+	ORIENTATION_90,
+	ORIENTATION_180,
+	ORIENTATION_270
+};
+
 #define CACHED_LINES 16
 // enough memory to hold 16 lines of the display for fast character drawing
-#define CACHE_SIZE (LCD_PITCH*CACHED_LINES)
+#define CACHE_SIZE (320*CACHED_LINES)
 // memory offset of visible area (80x160 out of 240x320)
-#define LCD_XOFFSET 0
-#define LCD_YOFFSET 24
 
 // Proportional font data taken from Adafruit_GFX library
 /// Font data stored PER GLYPH
@@ -38,12 +67,13 @@ typedef struct {
 } GFXfont;
 
 void lcdFill(uint16_t u16Color);
-void lcdInit(uint32_t u32Speed, uint8_t u8CSPin, uint8_t u8DCPin, uint8_t u8RSTPin, uint8_t u8BLPin);
+void lcdInit(int iLCDType, uint32_t u32Speed, uint8_t u8CSPin, uint8_t u8DCPin, uint8_t u8RSTPin, uint8_t u8BLPin);
 void lcdWriteCMD(uint8_t ucCMD);
 void lcdWriteDATA(uint8_t *pData, int iLen);
 int lcdWriteString(int x, int y, char *szMsg, uint16_t usFGColor, uint16_t usBGColor, int iFontSize);
 int lcdDrawTile(int x, int y, int iTileWidth, int iTileHeight, unsigned char *pTile, int iPitch);
 int lcdWriteStringCustom(GFXfont *pFont, int x, int y, char *szMsg, uint16_t usFGColor, uint16_t usBGColor, int bBlank);
+void lcdOrientation(int iOrientation);
 
 #define COLOR_BLACK 0
 #define COLOR_WHITE 0xffff
@@ -53,6 +83,11 @@ int lcdWriteStringCustom(GFXfont *pFont, int x, int y, char *szMsg, uint16_t usF
 #define COLOR_MAGENTA 0xf81f
 #define COLOR_CYAN 0x7ff
 #define COLOR_YELLOW 0xffe0
+
+// MADCTL flip bits
+#define MADCTL_YFLIP 0x80
+#define MADCTL_XFLIP 0x40
+#define MADCTL_VFLIP 0x20
 
 enum {
 	FONT_6x8 = 0,
